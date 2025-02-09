8cb35f5b90ab59eeb1f2b4676c2c4b0455f8f2c8
nishanth <n.ganesh@everteam.com>
2025-02-09 23:52:04 +0530

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,76 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { MessageModule } from 'primeng/message';
+import { CommonModule } from '@angular/common';
+import {Carousel} from 'primeng/carousel';
+import {Tag} from 'primeng/tag';
+import {Product} from './product';
+import {ProductService} from './products.service';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, InputTextModule, ButtonModule, MessageModule, FormsModule, Carousel, Tag],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  providers:  [ ProductService ]
+})
+export class AppComponent {
+  text = '';
+
+  msg = '';
+
+  products: any | undefined;
+
+  responsiveOptions: any[] | undefined;
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit() {
+    this.productService.getProductsSmall().then((products) => {
+      this.products = products;
+    });
+
+
+    this.responsiveOptions = [
+      {
+        breakpoint: '1400px',
+        numVisible: 2,
+        numScroll: 1,
+      },
+      {
+        breakpoint: '1199px',
+        numVisible: 3,
+        numScroll: 1,
+      },
+      {
+        breakpoint: '767px',
+        numVisible: 2,
+        numScroll: 1,
+      },
+      {
+        breakpoint: '575px',
+        numVisible: 1,
+        numScroll: 1,
+      },
+    ];
+  }
+
+  // public getSeverity(status: string) {
+  //   switch (status) {
+  //     case 'INSTOCK':
+  //       return 'success';
+  //     case 'LOWSTOCK':
+  //       return 'warn';
+  //     case 'OUTOFSTOCK':
+  //       return 'danger';
+  //   }
+  // }
+
+  onClick() {
+    this.msg = 'Welcome ' + this.text;
+  }
+}
